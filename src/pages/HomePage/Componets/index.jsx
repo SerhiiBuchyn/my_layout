06d08fdc324434dbc2ab2183ef166024006ba42d
SerhiiBuchyn <serhii.buchyn@gmail.com>
2023-01-21 16:57:56 +0200

    Update files construction in HomePage.jsx

---
+++ src/pages/HomePage/Componets/index.jsx
@@ -0,0 +1,11 @@
+export * from "./Hero/Hero";
+export * from "./AboutUs/AboutUs";
+export * from "./Dashboard/Dashboard";
+export * from "./OurServices/OurServices";
+export * from "./OurCustomers/index";
+
+export * from "./Hero/Hero.css";
+export * from "./AboutUs/AboutUs.css";
+export * from "./Dashboard/Dashboard.css";
+export * from "./OurServices/OurServices.css";
+export * from "./OurCustomers/OurCustomers.css";
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import Hero from "./Componets/Hero/Hero";
+import AboutUs from "./Componets/AboutUs/AboutUs";
+import Dashboard from "./Componets/Dashboard/Dashboard";
+import OurServices from "./Componets/OurServices/OurServices";
+
+import "./HomePage.css";
 
 const HomePage = () => {
-	return <div className="home__page">HomePage</div>;
+	return (
+		<div className="home__page">
+			<Hero />
+			<AboutUs />
+			<OurServices />
+			<Dashboard />
+		</div>
+	);
 };
 
 export default HomePage;
